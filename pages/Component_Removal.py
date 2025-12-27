5a11897bfb02d6f126b4572c9ac3a61072462484
GS Acharya <ezondiza@gmail.com>
2025-12-27 07:25:18 +0545

    Update Component_Removal.py

--- pages/Component_Removal.py
+++ pages/Component_Removal.py
@@ -1,8 +1,10 @@
+# /pages/Component_Removal.py
+
 import streamlit as st
 from forms.component_removal_form import component_removal_form
 
 st.set_page_config(page_title="Component Removal", layout="wide")
 
-st.title("Component Removal Entry")
+st.title("Component Removal")
 
 component_removal_form()
